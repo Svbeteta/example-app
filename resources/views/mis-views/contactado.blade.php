c7a91a6b7b3e59eba540fe51e5e04a5059bf4012
Samuel Beteta <sbeteta78@gmail.com>
2024-09-04 15:35:32 -0600

    Añadí un mensaje más elaborado a la hora de contactar al equipo VIP

--- resources/views/mis-views/contactado.blade.php
+++ resources/views/mis-views/contactado.blade.php
@@ -1,6 +1,6 @@
 @extends('my-layouts.my-app-layout')
 @section('content')
-    <h1> Gracias por Contactar VIP ✨ </h1>
-    <p>Apreciamos su feedback! 👌.</p>
+    <h1> ¡Gracias por Contactar a nuestro equipo VIP! ✨ </h1>
+    <p>Valoramos mucho su opinión y estamos comprometidos en ofrecerle el mejor servicio. Su feedback es esencial para seguir mejorando 👌.</p>
     
 @endsection
